94b6dedd0d2c6d2427b2364c6204b4cc0cbc2c75
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-05-02 21:41:54 -0400

    updated code

--- develop/script.js
+++ develop/script.js
@@ -2,24 +2,47 @@
 
 // prompts for password
 var charCount = window.prompt("How many characters would you like your password?");
-
-
-var upperLetter = window.prompt("Would you like an uppercase letter?");
-
-
-var lowerLetter = window.prompt("Would you like a lower case letter");
-
-
-var number = window.prompt("Would you like a number?");
-
-
-var symbols = window.prompt("Would you like a symbol?");
+    if (charCount < 8 || charCount > 128) {
+        window.alert("Your password has to be between 8 and 128 characters");
+    }
+    else {
+        var upperLetter = window.prompt("Would you like an uppercase letter?");
+        var lowerLetter = window.prompt("Would you like a lower case letter");
+        var number = window.prompt("Would you like a number?");
+        var symbols = window.prompt("Would you like a symbol?");
+    for(i =0; i<passwordLength; i++){
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      passwords+= chars.substring(randomNumber,randomNumber+1);
+  }
+}
 
 window.alert("Click the generate button below to generate a password");
 
-function passwordInitiate() {
-    window.alert("Click the Generate Button to generate your password!")
+function upperLetter() {
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (upperLetter = upper) {
+        return Math.floor(math.random() * 26);
+    }
 }
+
+function lowerLetter() {
+    var lower = "abcdefghijklmnopqrstuvwxyz";
+}
+
+function number() {
+    var num = "0123456789";
+}
+
+function symbols() {
+    var sym = "!@#$%^&*_-+=/?";
+}
+
+
+
+
+
+
+  
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -27,7 +50,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
